ec173062ba9e6f2ebd6205346738616a958232f0
bhayes7 <bhayes@sdsu.edu>
2019-06-19 15:20:59 -0700

    updated cleanup script to strip irrelevant, content-less endmatter (partially) out of documents

--- clean.py
+++ clean.py
@@ -26,7 +26,7 @@ import unicodedata
 import re
 
 def unicode_normalize(string):
-    text = unicodedata.normalize("NFKD", string.replace("\xad", "")) # unlist the paragraphs and remove unicode
+    text = unicodedata.normalize("NFKD", string.replace("\xad", "")) # normalize unicode
     text = re.sub(' +', ' ', text) # strip out any excess spaces
     return text
 
@@ -36,9 +36,31 @@ def clean_postText(post):
 def clean_keywords(kws):
     return [string.strip() for string in kws.split(",")]
 
+# many articles conclude with lists of other article titles, links, or other text 
+# that isn't actually part of the article. Identifying these precisely is difficult,
+# but we can get rid of some of them this way
+
+def strip_endmatter(paragraphs):
+    """ Remove content-less endmatter (links to other articles, etc.) from an article.
+    For now, removes all paragraphs from end of the article that aren't end-punctuated. 
+    This is clearly imperfect and I'll explore more sophisticated ways to identify this textual clutter.
+    
+    Args:
+        paragraphs (list of strings): the article as a list of strings, in order
+        
+    Returns:
+        list of strings: the article with endmatter removed
+    """
+    # the intuition is that content-less endmatter won't be properly punctuated most of the time
+    regex = re.compile("[\.\?!\"'’”]$") # sentence-final punctuation
+    open_ended = [bool(regex.findall(p)) for p in paragraphs]
+    open_ended.reverse()
+    return paragraphs[:len(paragraphs) - open_ended.index(True)]
+
 def clean_paragraphs(ps):
-    # paragraphs are given as lists of strings, so join them with newline character
-    return unicode_normalize("\n".join(ps)) 
+    normalized = [unicode_normalize(p) for p in ps] # normalize unicode
+    stripped = strip_endmatter(normalized) # remove useless endmatter
+    return "\n".join(stripped) # paragraphs are given as lists of strings, so join them with newline character
 
 df['postText'] = df['postText'].apply(clean_postText)
 df['targetKeywords'] = df['targetKeywords'].apply(clean_keywords)
