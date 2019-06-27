8f390beb77b66e79178f1bf16d1b8ecf5f253c07
bhayes7 <bhayes@sdsu.edu>
2019-06-27 15:13:34 -0700

    added even more plots

--- dim_stats.R
+++ dim_stats.R
@@ -66,3 +66,60 @@ sums <- stats %>%
 
 ggplot(sums, aes(x = sum, y = proportion)) +   
   geom_bar(aes(fill = truthClass), position = "dodge", stat="identity")
+
+features <- stats %>%
+  select(id, truthClass, listicle, leadsWithQuestion, hasDeterminer, superlative, accusatory) %>%
+  mutate(sum = rowSums(.[3:7])) %>%
+  gather(key = "feature", value = "present", listicle:accusatory)
+
+one_feature <- features %>%
+  filter(sum == 1) %>%
+  filter(present == 1) %>%
+  group_by(truthClass) %>%
+  count(feature) %>%
+  add_tally() %>%
+  mutate(proportion = n / nn)
+
+ggplot(one_feature, aes(x = feature, y = proportion)) +   
+  geom_bar(aes(fill = truthClass), position = "dodge", stat="identity")
+
+two_feature <- features %>%
+  filter(sum == 2) %>%
+  filter(present ==  1) %>%
+  spread(feature, feature) %>%
+  replace_na(list(accusatory = "", hasDeterminer = "", leadsWithQuestion = "", listicle = "", superlative = "")) %>%
+  unite(col = "features", accusatory:superlative, sep = " ") %>%
+  group_by(truthClass) %>%
+  count(features) %>%
+  add_tally() %>%
+  mutate(proportion = n / nn)
+
+ggplot(two_feature, aes(x = features, y = proportion)) +   
+  geom_bar(aes(fill = truthClass), position = "dodge", stat="identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+three_feature <- features %>%
+  filter(sum == 3) %>%
+  filter(present ==  1) %>%
+  spread(feature, feature) %>%
+  replace_na(list(accusatory = "", hasDeterminer = "", leadsWithQuestion = "", listicle = "", superlative = "")) %>%
+  unite(col = "features", accusatory:superlative, sep = " ") %>%
+  group_by(truthClass) %>%
+  count(features) %>%
+  add_tally() %>%
+  mutate(proportion = n / nn)
+
+ggplot(three_feature, aes(x = features, y = proportion)) +   
+  geom_bar(aes(fill = truthClass), position = "dodge", stat="identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+four_feature <- features %>%
+  filter(sum == 4) %>%
+  filter(present ==  1) %>%
+  spread(feature, feature) %>%
+  replace_na(list(accusatory = "", hasDeterminer = "", leadsWithQuestion = "", listicle = "", superlative = "")) %>%
+  unite(col = "features", accusatory:superlative, sep = " ") %>%
+  group_by(truthClass) %>%
+  count(features) %>%
+  add_tally() %>%
+  mutate(proportion = n / nn)
