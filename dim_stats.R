5a8358f2c9f896170bb8c754d62da548c4ddf52f
bhayes7 <bhayes@sdsu.edu>
2019-06-27 11:49:43 -0700

    added better plots to R script

--- dim_stats.R
+++ dim_stats.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 stats <- read.csv("dim_stats.csv")
 
@@ -8,27 +10,48 @@ clickbait <- stats %>%
 not_clickbait <- stats %>%
   filter(truthClass == "no-clickbait")
 
+metrics <- stats %>%
+  group_by(truthClass) %>%
+  gather(key = "stat", value = "value", polarity:modality)
+
+ggplot(metrics, aes (x = value)) +
+  geom_histogram(bins = 20) +
+  facet_wrap(truthClass~stat)
+
 avgs <- stats %>%
   group_by(truthClass) %>%
   summarise(
-    pol_mean = mean(pol),
-    pol_sd = sd(pol),
-    subj_mean = mean(subj),
-    subj_sd = sd(subj),
-    mod_mean = mean(mod),
-    mod_sd = sd(mod)
-  )
+    pol_mean = mean(polarity),
+    subj_mean = mean(subjectivity),
+    mod_mean = mean(modality)
+  ) %>%
+  gather(key = "stat", value = "value", pol_mean:mod_mean)
+
+ggplot(avgs, aes(x = truthClass, y = value)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~stat)
 
 counts <- stats %>%
   group_by(truthClass) %>%
   summarise(
     listicle = sum(listicle) / n(),
     leadsWithQuestion = sum(leadsWithQuestion) / n(),
-    hasDet = sum(hasDet) / n(),
-    superlative = sum(sup) / n()
-  )
+    hasDeterminer = sum(hasDeterminer) / n(),
+    superlative = sum(superlative) / n(),
+    accusatory = sum(accusatory) / n()
+  ) %>%
+  gather(key = "stat", value = "proportion", listicle:accusatory)
 
-t.test(clickbait$pol, not_clickbait$pol, alternative = "two.sided")
-t.test(clickbait$subj, not_clickbait$subj, alternative = "two.sided")
-t.test(clickbait$mod, not_clickbait$mod, alternative = "two.sided")
+ggplot(counts, aes(x = truthClass, y = proportion)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~stat)
 
+ents <- stats %>%
+  group_by(truthClass) %>%
+  count(numNamedEntities) %>%
+  add_tally() %>%
+  mutate(proportion = n / nn) %>%
+  filter(numNamedEntities < 7)
+
+ggplot(ents, aes(x = numNamedEntities, y = proportion)) +   
+  geom_bar(aes(fill = truthClass), position = "dodge", stat="identity")
